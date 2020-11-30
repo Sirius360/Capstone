0e6f3d89d28db90b2b8eb9b35bc9b1d3c74e903c
Quynh Nguyen Van <qnv164@gmail.com>
2020-11-30 22:15:54 +0700

    fix layout

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -3,18 +3,25 @@
 @section('content')
 
     <div class="container">
-        <form>
-            <div class="form-group">
-                <input class="form-control fs-0" type="email" placeholder="Email Address" name="forgot-password-email" />
+        <div class="row justify-content-center">
+            <div class="col-xl-5 col-lg-6 col-md-7">
+                <div class="text-center">
+                    <h2>Forgot password &#x1f62B;</h2>
+                    <p class="lead">Enter your email address to reset</p>
+                    <form>
+                        <div class="form-group">
+                            <input class="form-control fs-0" type="email" placeholder="Email Address" name="forgot-password-email" />
+                        </div>
+                        <button class="btn btn-lg btn-block btn-primary" role="button" type="submit">
+                            Send reset link
+                        </button>
+                    </form>
+                    <div class="mt-2">
+                        <a class="fs-0" href="{{ url('/login') }}"><span class="pallette-grey-4">&larr; Back to Login</span></a>
+                    </div>
+                </div>
             </div>
-            <button class="btn btn-lg btn-block btn-primary" role="button" type="submit">
-                Send reset link
-            </button>
-        </form>
-        <div class="mt-2">
-            <a class="fs-0" href="{{URL::to('/login')}}"><span class="pallette-grey-4">&larr; Back to Login</span></a>
         </div>
-
     </div>
 
 @endsection
